cbeeb3f1c9dc142fc69f09ec3f4ee0c136fa028d
Oscar Katz <ogkatz@googlemail.com>
2024-06-27 18:43:29 +0100

    Some handling of CORS requests

--- lambda_function.py
+++ lambda_function.py
@@ -1,13 +1,13 @@
 import typing as t
 
-from pydantic import BaseModel
+from pydantic import BaseModel, ValidationError
 
 from methods import methods, MethodError
 
 class JRPCRequest(BaseModel):
     jsonrpc: t.Literal["2.0"]
     method: str
-    params: tuple[str, ...]
+    params: list[str]
     id: int
 
 class JRPCResponse(BaseModel):
@@ -20,16 +20,38 @@ class JRPCError(BaseModel):
     error: str
     id: int
 
-def lambda_handler(event: dict, context) -> JRPCResponse | JRPCError | None:
-    try:
-        request_obj = JRPCRequest.model_validate(event)
+def serialise_object(func: t.Callable[..., BaseModel | None]) -> t.Callable[..., dict | None]:
+    def inner(*args, **kwargs):
+        ret = func(*args, **kwargs)
+
+        if ret:
+            dmp = ret.model_dump()
+            print(dmp)
+            return dmp
+        else:
+            return None
     
-    except:
-        return None
+    return inner
+
+@serialise_object
+def lambda_handler(event: dict, context) -> JRPCResponse | JRPCError | None:    
+    try:
+        event = event["body"]
+    except KeyError:
+        event = event
+
+    try:
+        request_obj = JRPCRequest.model_validate_json(event)
+    
+    except ValidationError as e:
+        return JRPCError(
+            error=f"Could not validate event: {event} due to {e}",
+            id=0,
+        )
 
     if method := methods.get(request_obj.method, None):
         try:
-            ret = method(request_obj.params)
+            ret = method(*request_obj.params)
 
             return JRPCResponse(
                 result=ret,
@@ -48,3 +70,6 @@ def lambda_handler(event: dict, context) -> JRPCResponse | JRPCError | None:
             id=request_obj.id
         )
 
+if __name__=="__main__":
+    breakpoint()
+    pass
